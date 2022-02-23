7650d79f585e36e7d8848dad0165cef101780dc4
pengchen <pc20131001@gmail.com>
2022-02-23 13:38:17 +0800

    feat:添加测试

--- src/main/java/online/proyi/o2o3/dao/repository/AreaRepository.java
+++ src/main/java/online/proyi/o2o3/dao/repository/AreaRepository.java
@@ -4,6 +4,11 @@ import online.proyi.o2o3.entity.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotBlank;
+import java.util.Optional;
+
 @Repository
 public interface AreaRepository extends JpaRepository<Area, Integer> {
+
+    Optional<Area> findByName(@NotBlank String name);
 }
--- src/test/java/online/proyi/o2o3/controller/superAdmin/AreaControllerTest.java
+++ src/test/java/online/proyi/o2o3/controller/superAdmin/AreaControllerTest.java
@@ -2,6 +2,7 @@ package online.proyi.o2o3.controller.superAdmin;
 
 import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
+import online.proyi.o2o3.dao.mapper.AreaMapper;
 import online.proyi.o2o3.dao.repository.AreaRepository;
 import online.proyi.o2o3.entity.Area;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,8 @@ import org.springframework.util.Assert;
 class AreaControllerTest {
     @Autowired
     AreaRepository areaRepository;
+    @Autowired
+    AreaMapper areaMapper;
 
     @Test
     public void AreaInsertTest() {
@@ -22,7 +25,28 @@ class AreaControllerTest {
         area.setPriority(1);
         Area dbArea = areaRepository.save(area);
         log.info("db返回值:{}", JSONUtil.toJsonStr(dbArea));
-        Assert.isTrue(dbArea.getName().equals("地区1"), "存储方法有误!");
+        Assert.isTrue(dbArea.getName().equals("地区1"), "地区保存方法有误!");
     }
 
+    @Test
+    public void AreaSelectTest() {
+        Area dbArea = areaRepository.findByName("地区1").orElse(null);
+        log.info("db返回值:{}", JSONUtil.toJsonStr(dbArea));
+        Assert.isTrue(dbArea.getName().equals("地区1"), "地区查询方法有误!");
+    }
+
+    @Test
+    public void AreaUpdateTest() {
+        Area dbArea = areaRepository.findByName("地区1").orElse(null);
+        dbArea.setPriority(2);
+        Area updateArea = areaRepository.save(dbArea);
+        log.info("db返回值:{}", JSONUtil.toJsonStr(updateArea));
+        Assert.isTrue(updateArea.getPriority().equals(2), "地区更新方法有误!");
+    }
+
+    @Test
+    public void AreaLoginDeleteTest() {
+        int result = areaMapper.deleteById(1);
+        Assert.isTrue(result > 0 , "地区逻辑删除方法有误!");
+    }
 }
